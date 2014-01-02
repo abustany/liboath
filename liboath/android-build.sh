cad04cea63b3a0a52f99b556de91f4c4c0dddbc2
Adrien Bustany <adrien.bustany@nokia.com>
2014-01-02 21:07:14 +0100

    Fix Android build to be static and not require gcc 4.8

--- liboath/android-build.sh
+++ liboath/android-build.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $0)
 rm -fr generated libs obj
-make -f ~/.local/android-studio/android-ndk-r9c/build/core/build-local.mk APP_BUILD_SCRIPT=Android.mk NDK_TOOLCHAIN=arm-linux-androideabi-4.8 V=1
+make -f ~/.local/android-studio/android-ndk-r9c/build/core/build-local.mk APP_BUILD_SCRIPT=Android.mk V=1
